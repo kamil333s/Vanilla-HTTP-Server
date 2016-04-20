44730b817dcef752356aaf9d8db0549a7b69c5fa
Kevin Smith <kamil@sisna.com>
2016-04-20 11:10:29 -0700

    Linted,uncomment POST, tests work

--- Kevinsmith/test/server_test.js
+++ Kevinsmith/test/server_test.js
@@ -1,41 +1,37 @@
 var chai = require('chai');
 var chaiHTTP = require('chai-http');
 chai.use(chaiHTTP);
-var fs = require('fs');
+// var fs = require('fs');
 
-var request = chai.HTTP;
+var request = chai.request;
 var expect = chai.expect;
 require(__dirname + '/../server');
 
 describe('Vanilla HTTP server tests', () => {
-// var html;
-// before(() => {
-// fs.readFile(__dirname + '/../public/index.html', (err, data)) ************************************
-//   html = data.toString();
-//   done();
-// })
 
   it('should respond to /time with the current time', (done) => {
     request('localhost:3000')
-    .get('/time');
+    .get('/time')
     .end((err, res) => {
-      expect(err).to.equl(null);
+      expect(err).to.eql(null);
       expect(res).have.status(200);
       // expect(res).to.have.header('content-type'. 'application/json');
-      expect(res.text).to.match('The time is ');
+      // console.log('res.text:', res.text);
+      expect(res.text).to.contain('The time is ');
       done();
-    })
-  })
+    });// end
+  });// it
   
-  it('/greet/kevin should send back "kevin, Greetings and Felicitations! Hip Hip Hoorah. Tallyho!''', (done) =>{
+  it('/greet/kevin should send back "kevin, Greetings and Felicitations! Hip Hip Hoorah. Tallyho!"', (done) =>{
     request('localhost:3000')
     .get('/greet/kevin')
     .end((err, res) => {
-      expect(err).to.equl(null);
+      expect(err).to.eql(null);
       expect(res).have.status(200);
       expect(res.text).to.eql('kevin, Greetings and Felicitations! Hip Hip Hoorah. Tallyho!');
       done();
-    })
+    });// end
+  });// it
 
   it('should send back an error page', (done) =>{
     request('localhost:3000')
@@ -45,7 +41,7 @@ describe('Vanilla HTTP server tests', () => {
       expect(res).have.status(404);
       expect(res.text).to.eql('404- file not found');
       done();
-    })
+    });// end
+  });// it
 
-  })
-})
+});// describe
